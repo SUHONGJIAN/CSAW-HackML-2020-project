afb90d7c92ef9be36ad8c076d022345464f3bfa8
hongjian su <hongjiansu@HongjiandeMacBook-Pro.local>
2020-12-24 00:59:27 +0800

    hotfix

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 ## Introduction
-A backdoor detector for BadNets trained on the YouTube Face dataset.  
+This is a backdoor detector for BadNets trained on the YouTube Face dataset.  
 BadNets: all BadNets are under **models/** directory  
 Repaired Models: under **entropy/** and **finePruning/** directory
 Methods: STRIP + Fine-pruning
-## Environment Requirements:
+## Environment Requirements
  - python 3.x
  - os
  - numpy
  - keras
+ - tensorflow
  - h5py
  - scipy
  - matplotlib
@@ -17,3 +18,8 @@ Methods: STRIP + Fine-pruning
 2. Open your terminal and run:  
     > *python eval.py*
 3. Follow the instructions from your terminal
+
+## **Attention!!!**
+***In the pruning process,  the running time may be very long. (e.g. for 10k data, may need 15min.)***  
+***If you notice the following warnings from your terminal, please ignore them.***  
+![](resources/warning.png)
--- eval.py
+++ eval.py
@@ -2,6 +2,7 @@ import keras
 import h5py
 import numpy as np
 import os.path
+import tensorflow as tf
 from strip import STRIP
 
 """
@@ -24,67 +25,115 @@ class DataLoader:
 
 def num_to_net(num):
     numbers = {
-        "1": {"model": "models/sunglasses_bd_net.h5", "entropy": "entropy/entropy_clean_sunglasses.h5", "fine_pruned_model": ""},
-        "2": {"model": "models/anonymous_1_bd_net.h5", "entropy": "entropy/entropy_clean_anonymous1.h5", "fine_pruned_model": ""},
-        "3": {"model": "models/anonymous_2_bd_net.h5", "entropy": "entropy/entropy_clean_anonymous2.h5", "fine_pruned_model": ""},
-        "4": {"model": "models/multi_trigger_multi_target_bd_net.h5", "entropy": "entropy/entropy_clean_multi.h5", "fine_pruned_model": ""},
+      "1": {"model": "models/sunglasses_bd_net.h5", "entropy": "entropy/entropy_clean_sunglasses.h5", "fine_pruned_model": "finePruning/sunglasses_repaired_net.h5"},
+      "2": {"model": "models/anonymous_1_bd_net.h5", "entropy": "entropy/entropy_clean_anonymous1.h5", "fine_pruned_model": "finePruning/anonymous_1_repaired_net.h5"},
+      "3": {"model": "models/anonymous_2_bd_net.h5", "entropy": "entropy/entropy_clean_anonymous2.h5", "fine_pruned_model": "finePruning/anonymous_2_repaired_net.h5"},
+      "4": {"model": "models/multi_trigger_multi_target_bd_net.h5", "entropy": "entropy/entropy_clean_multi.h5", "fine_pruned_model": "finePruning/multi_trigger_multi_target_repaired_net.h5"},
     }
-    return numbers.get(num, {"model": "models/sunglasses_bd_net.h5", "entropy": "entropy/entropy_clean_sunglasses.h5"})
+    return numbers.get(num, {"model": "models/sunglasses_bd_net.h5", "entropy": "entropy/entropy_clean_sunglasses.h5", "fine_pruned_model": "finePruning/sunglasses_repaired_net.h5"})
 
+def num_to_mode(num):
+    numbers = {
+      "1": "seperate",
+      "2": "mixed",
+    }
+    return numbers.get(num, "seperate")
 
 def main():
-    # Choose a test BadNet
+    # Select a test BadNet
     print('\033[0;32m' + "Select a test net: \n1. sunglasses_bd_net \n2. anonymous_1_bd_net \n3. anonymous_2_bd_net \n4. multi-trigger_multi_target_bd_net")
     net = num_to_net(input())
     model = net["model"]
-    print("{0} selected!".format(model))
+    print("{0} selected!\n".format(model))
     bd_model = keras.models.load_model(model)
 
-    # Set the test poisoned data
-    print("Please put the corresponding poisoned data under eval/ and name the file poisoned.h5 (i.e. eval/poisoned.h5) \nThen click enter.")
-    input()
-    clean_data_test_filename = "data/clean_test_data.h5"
-    poisoned_data_test_filename = "eval/poisoned.h5"
-    while not os.path.isfile(poisoned_data_test_filename):
-        print('\033[91m' + "Error: eval/poisoned.h5 does not exist. please try again.\nThen click enter.")
+    # Select the test mode
+    print("Select the test mode: \n1. Seperate data (clean / poisoned) \n2. Mixed data")
+    test_mode = num_to_mode(input())
+    if test_mode == "seperate":
+        print("Seperate mode selected!\n")
+        # Set the test poisoned data
+        print("Please put the poisoned data under data/ and name the file poisoned_data.h5 (i.e. data/poisoned_data.h5) \nThen click enter.")
         input()
-    test_clean = DataLoader(clean_data_test_filename)
-    test_poisoned = DataLoader(poisoned_data_test_filename)
+        poisoned_data_test_filename = "data/poisoned_data.h5"
+        while not os.path.isfile(poisoned_data_test_filename):
+            print('\033[91m' + "Error: data/poisoned_data.h5 does not exist. please try again.\nThen click enter.")
+            input()
+        # Set the test clean data
+        print('\033[0;32m' + "Please put the clean data under data/ and name the file clean_data.h5 (i.e. data/clean_data.h5) \nThen click enter.")
+        input()
+        clean_data_test_filename = "data/clean_data.h5"
+        while not os.path.isfile(poisoned_data_test_filename):
+            print('\033[91m' + "Error: data/clean_data.h5 does not exist. please try again.\nThen click enter.")
+            input()
+        test_clean = DataLoader(clean_data_test_filename)
+        test_poisoned = DataLoader(poisoned_data_test_filename)
 
-    # Step one: STRIP
-    print('\033[0;32m' + "STRIP: running......(maybe several minutes)")
-    entropy_filename = net["entropy"]
-    entropy_clean_data = h5py.File(entropy_filename, "r")
-    entropy_clean = np.asarray(entropy_clean_data["data"])
-    STRIP_filter = STRIP(50, 1.5, 1, 0)
-    pred_poisoned = STRIP_filter.predict(entropy_clean, test_poisoned.x, bd_model)
-    succ_att_rate = np.mean(np.equal(pred_poisoned, test_poisoned.y)) * 100
-    print('\033[95m' + "Success attack rate after STRIP: {0}%".format(succ_att_rate))
-    pred_clean = STRIP_filter.predict(entropy_clean, test_clean.x, bd_model)
-    accu = np.mean(np.equal(pred_clean, test_clean.y)) * 100
-    print("Accuracy after STRIP: {0}%".format(accu))
+        # Step one: STRIP
+        print('\033[0;32m' + "STRIP: running......(maybe several minutes, even hours)")
+        entropy_filename = net["entropy"]
+        entropy_clean_data = h5py.File(entropy_filename, "r")
+        entropy_clean = np.asarray(entropy_clean_data["data"])
+        STRIP_filter = STRIP(50, 1.5, 1, 0)
+        pred_poisoned = STRIP_filter.predict(entropy_clean, test_poisoned.x, bd_model)
+        succ_att_rate = np.mean(np.equal(pred_poisoned, test_poisoned.y)) * 100
+        print('\033[95m' + "Success attack rate after STRIP: {0}%".format(succ_att_rate))
+        print('\033[0;32m' + "Please wait for accuracy...")
+        pred_clean = STRIP_filter.predict(entropy_clean, test_clean.x, bd_model)
+        accu = np.mean(np.equal(pred_clean, test_clean.y)) * 100
+        print("Accuracy after STRIP: {0}%".format(accu))
 
-    # Step two: Fine-pruning
-    print('\033[0;32m' + "Fine-pruning: running......")
-    I_poisoned_remaining = np.argwhere(pred_poisoned == test_poisoned.y)
-    test_poisoned_remaining = test_poisoned.x[I_poisoned_remaining]
-    I_clean_remaining = np.argwhere(pred_clean != test_clean.y)
-    test_clean_remaining = test_clean.x[I_clean_remaining]
-    fine_pruned_model = keras.models.load_model(net["fine_pruned_model"])
-    pred_poisoned_remaining = fine_pruned_model.predict(test_poisoned_remaining)
-    succ_att_rate = (np.sum(pred_poisoned_remaining == test_poisoned.y[I_poisoned_remaining]) / test_poisoned.y.shape[0]) * 100
-    print('\033[95m' + "Success attack rate after fine-pruning: {0}%".format(succ_att_rate))
-    pred_clean_remaining = fine_pruned_model.predict(test_clean_remaining)
-    accu = (test_clean.y.shape[0] - np.sum(pred_clean_remaining != test_clean.y[I_clean_remaining]) / test_clean.y.shape[0]) * 100
-    print("Accuracy after fine-pruning: {0}%".format(accu))
+        # Step two: Fine-pruning
+        print('\033[0;32m' + "Fine-pruning: running......")
+        I_poisoned_remaining = np.argwhere(pred_poisoned == test_poisoned.y)
+        test_poisoned_remaining = test_poisoned.x[I_poisoned_remaining]
+        I_clean_remaining = np.argwhere(pred_clean != test_clean.y)
+        test_clean_remaining = test_clean.x[I_clean_remaining]
+        fine_pruned_model = keras.models.load_model(net["fine_pruned_model"])
+        pred_poisoned_remaining = fine_pruned_model.predict(test_poisoned_remaining)
+        succ_att_rate = (np.sum(pred_poisoned_remaining == test_poisoned.y[I_poisoned_remaining]) / test_poisoned.y.shape[0]) * 100
+        print('\033[95m' + "Success attack rate after fine-pruning: {0}%".format(succ_att_rate))
+        print('\033[0;32m' + "Please wait for accuracy...")
+        pred_clean_remaining = fine_pruned_model.predict(test_clean_remaining)
+        accu = (test_clean.y.shape[0] - np.sum(pred_clean_remaining != test_clean.y[I_clean_remaining]) / test_clean.y.shape[0]) * 100
+        print("Accuracy after fine-pruning: {0}%".format(accu))
 
-    # Output the final prediction
-    for i in I_poisoned_remaining:
-      pred_poisoned[i] = pred_poisoned_remaining[i]
-    print("Final prediction of poisoned data: {0}".format(pred_poisoned))
-    for i in I_clean_remaining:
-      pred_clean[i] = pred_clean_remaining[i]
-    print("Final prediction of clean data: {0}".format(pred_poisoned))
+         # Output the final prediction
+        for i in I_poisoned_remaining:
+          pred_poisoned[i] = pred_poisoned_remaining[i]
+        print("Final prediction of poisoned data: {0}".format(pred_poisoned))
+        for i in I_clean_remaining:
+          pred_clean[i] = pred_clean_remaining[i]
+        print("Final prediction of clean data: {0}".format(pred_poisoned))
+    else:
+        # Set the test mixed data
+        print("Please put the mixed data under data/ and name the file mixed_data.h5 (i.e. data/mixed_data.h5) \nThen click enter.")
+        input()
+        mixed_data_test_filename = "data/mixex_data.h5"
+        while not os.path.isfile(mixed_data_test_filename):
+            print('\033[91m' + "Error: data/mixed_data.h5 does not exist. please try again.\nThen click enter.")
+            input()
+        test_mixed = DataLoader(mixed_data_test_filename)
+
+        # Step one: STRIP
+        print('\033[0;32m' + "STRIP: running......(maybe several minutes, even hours)")
+        entropy_filename = net["entropy"]
+        entropy_clean_data = h5py.File(entropy_filename, "r")
+        entropy_clean = np.asarray(entropy_clean_data["data"])
+        STRIP_filter = STRIP(50, 1.5, 1, 0)
+        pred_mixed = STRIP_filter.predict(entropy_clean, test_mixed.x, bd_model)
+
+        # Step two: Fine-pruning
+        print('\033[0;32m' + "Fine-pruning: running......")
+        I_mixed_remaining = np.argwhere(pred_mixed == test_mixed.y)
+        test_mixed_remaining = test_mixed.x[I_mixed_remaining]
+        fine_pruned_model = keras.models.load_model(net["fine_pruned_model"])
+        pred_mixed_remaining = fine_pruned_model.predict(test_mixed_remaining)
+
+        # Output the final prediction
+        for i in I_mixed_remaining:
+          pred_mixed[i] = pred_mixed_remaining[i]
+        print("Final prediction of poisoned data: {0}".format(pred_mixed))
 
 
 if __name__ == "__main__":
